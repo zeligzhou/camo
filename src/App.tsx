2d8def1ccd24d8b96680999a857b106786292326
zhou <zeligzhou@gmail.com>
2022-05-09 15:19:25 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello!</p>
+        <p>
+          Camo is loading
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
